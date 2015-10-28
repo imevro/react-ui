0ae8812be9e0db73b2f3247e78aa3e37330ea567
Evgeniya Molodetskaya <march213@yahoo.com>
2015-10-28 14:50:11 +0300

    fix: image bordered styles

--- example/containers/app.jsx
+++ example/containers/app.jsx
@@ -3,6 +3,6 @@ import UI from 'components/ui';
 
 export default () => {
   return (
-    <UI.Image kind="circle" responsive src="https://images.unsplash.com/photo-1445251836269-d158eaa028a6?fit=crop&fm=jpg&h=800&q=80&w=1200" alt="image" title="title" />
+    <UI.Image kind="bordered" src="https://images.unsplash.com/photo-1445251836269-d158eaa028a6?fit=crop&fm=jpg&h=800&q=80&w=1200" alt="image" title="title" width="140" height="140" />
   );
 };
