9a89904735aeab0d7dd12473b50b5da634883df5
Evgeniya Molodetskaya <march213@yahoo.com>
2015-10-28 14:03:56 +0300

    fix: grid

--- example/containers/app.jsx
+++ example/containers/app.jsx
@@ -3,8 +3,32 @@ import UI from 'components/ui';
 
 export default () => {
   return (
-    <section>
-      <UI.Badges kind="primary">1</UI.Badges>
-    </section>
+    <UI.Container fluid>
+      <UI.Row>
+        <UI.Col xs="12" sm="6" md="4" lg="3">
+          first
+        </UI.Col>
+        <UI.Col xs="12" sm="6" md="4" lg="3">
+          second
+        </UI.Col>
+        <UI.Col xs="12" sm="6" md="4" lg="3">
+          third
+        </UI.Col>
+        <UI.Col xs="12" sm="6" md="4" lg="3">
+          fourth
+        </UI.Col>
+      </UI.Row>
+      <UI.Row>
+        <UI.Col xs="12" sm="6" md="4">
+          first
+        </UI.Col>
+        <UI.Col xs="12" sm="6" md="4">
+          second
+        </UI.Col>
+        <UI.Col xs="12" sm="6" md="4">
+          third
+        </UI.Col>
+      </UI.Row>
+    </UI.Container>
   );
 };
