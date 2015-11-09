38980558e3e1310f7db371edccfbe64070cbb84b
Eugene Rodionov <git@theaqua.im>
2015-11-09 16:57:27 +0300

    feat: add offsets to Grid.Col

--- src/components/grid/col.jsx
+++ src/components/grid/col.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import cn from 'classnames';
 
 export default (styles = {}) => props => {
-  const { xs, sm, md, lg } = props;
+  const {
+    xs, xsOffset,
+    sm, smOffset,
+    md, mdOffset,
+    lg, lgOffset,
+  } = props;
 
   const className = {
     [styles[`xs-${xs}`]]: !!xs,
+    [styles[`xs-offset-${xsOffset}`]]: !!xsOffset,
     [styles[`sm-${sm}`]]: !!sm,
+    [styles[`xs-offset-${smOffset}`]]: !!smOffset,
     [styles[`md-${md}`]]: !!md,
+    [styles[`xs-offset-${mdOffset}`]]: !!mdOffset,
     [styles[`lg-${lg}`]]: !!lg,
+    [styles[`xs-offset-${lgOffset}`]]: !!lgOffset,
   };
 
   return (
