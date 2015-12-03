fb8cdfd805d4751a1a5f4cf2924f9b1bedec9ae4
Evgeniya Molodetskaya <march213@yahoo.com>
2015-12-03 13:41:42 +0300

    fix: refactor

--- example/containers/app.jsx
+++ example/containers/app.jsx
@@ -7,9 +7,11 @@ export default () => {
         <UI.Grid.Container>
           <UI.Grid.Row>
             <UI.Grid.Col xs="6" xsOffset="3" style={{ marginTop: `50px` }}>
-              <UI.Form.Input prependWidth="2rem" appendWidth="4rem" block type="text" kind="primary" placeholder="Evgeniya"
+              <UI.Form.Input block type="text" kind="primary" placeholder="Evgeniya"
                 append={<UI.Icon icon="twitter" />}
+                appendWidth="4rem"
                 prepend={<UI.Icon icon="facebook" />}
+                prependWidth="2rem"
               />
             </UI.Grid.Col>
           </UI.Grid.Row>
