ce23c7f287e87428fe723ea12800d67bd2664cca
Konstantin Grushetsky <toxickg@openmailbox.org>
2016-05-24 04:00:28 +0300

    chore: get rid of 'Link' component, switch to exact version of React (0.14.8)

--- src/components/navbar/brand.jsx
+++ src/components/navbar/brand.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router';
 import cn from 'classnames';
 
-export default (styles = {}) => (props) => {
+export default (styles = {}) => props => {
   return (
-    <Link {...props} className={cn(styles.brand, props.className)} to={props.to}>
+    <span {...props} className={cn(styles.brand, props.className)}>
       {props.children}
-    </Link>
+    </span>
   );
 };
