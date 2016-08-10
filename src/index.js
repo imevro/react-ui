2e2c934852fdfd93c53899e43f6fd72c3544dc35
Konstantin Grushetsky <toxickg@icloud.com>
2016-08-10 15:08:52 +0300

    feat: remove basic components, refactor initialization logic

--- src/index.js
+++ src/index.js
@@ -1,19 +1,46 @@
 import camelCase from 'lodash/camelCase';
 
-const recursiveInitComponent = (component, styles, initializedUI, keyPath = []) => {
+/**
+ * Recursively initialize a components hierarchy
+ * @param  {Object|Function} component             [Function if a leaf component is supplied, Object if a wrapper component is detected]
+ * @param  {Object}          styles                [Global styles object]
+ * @param  {Object}          parentLayerComponents [An object of already initialized components from parent layer]
+ * @param  {Array}           keyPath               [Path to current component]
+ * @return {Object}                                [An object containing initialized component or components hierarchy]
+ */
+const initComponent = (component, styles, parentLayerComponents, keyPath = []) => {
   if (typeof component !== `function`) {
     return Object.keys(component).reduce((result, componentKey) => ({
       ...result,
-      [componentKey]: recursiveInitComponent(component[componentKey], styles, initializedUI, [...keyPath, componentKey]),
+      [componentKey]: initComponent(component[componentKey], styles, parentLayerComponents, [...keyPath, componentKey]),
     }), {});
   } else {
-    return component(styles[camelCase(keyPath[0])] || {}, initializedUI);
+    return component(styles[camelCase(keyPath[0])] || {}, parentLayerComponents);
   }
 };
 
-const init = (styles = {}) => (...uiLayers) => uiLayers.reduce((initializedUI, layer) => ({
-  ...initializedUI,
-  ...recursiveInitComponent(layer, styles, initializedUI),
+/**
+ * Recursive initialization of a layer
+ * @param  {Object} layer                 [Components object related to current UI abstraction layer]
+ * @param  {Object} styles                [Global styles object]
+ * @param  {Object} parentLayerComponents [An object of already initialized components from parent layer]
+ * @return {Object}                       [An object containing initialized components for current layer]
+ */
+const initLayer = (layer, styles, parentLayerComponents) => {
+  return Object.keys(layer).reduce((layerComponents, componentKey) => ({
+    ...layerComponents,
+    [componentKey]: initComponent(layer[componentKey], styles, parentLayerComponents, [componentKey]),
+  }), {});
+};
+
+/**
+ * React UI initialization
+ * @param  {Object} styles [Styles for every component]
+ * @return {Object}        [UI object that contains initialized components]
+ */
+const init = (styles = {}) => (...uiLayers) => uiLayers.reduce((initializedComponents, layer) => ({
+  ...initializedComponents,
+  ...initLayer(layer, styles, initializedComponents),
 }), {});
 
 export default init;
